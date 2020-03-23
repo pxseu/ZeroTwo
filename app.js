28764b386a35f5aed36fa761c262abba81baf5f4
pxseu <57842793+pxseu@users.noreply.github.com>
2020-03-23 10:03:02 +0100

    update

--- app.js
+++ app.js
@@ -143,7 +143,24 @@ client.on('message', async message => {
       });
       return 0;
 */
-//////////////////////////////////////////////////////////////////
+       
+//////////////////MISC////////////////////////////////////////////
+      
+    case "darling":
+      if (message.member.voiceChannel) {
+        message.member.voiceChannel.join()
+         .then(connection => {
+	   const dispatcher = connection.playFile('./darling.mp3');
+           dispatcher.on("end", end => {message.member.voiceChannel.leave();});
+      }).catch(console.log);
+      } else {
+        message.reply('You need to join a voice channel first!');
+      }
+      
+      return 0;
+                                                                 
+//////////////////MISC////////////////////////////////////////////
+
     case "vct":
       vctest(message);
       return 0;
