2cdc6e5c64e41184c0f8e7e7ec93d74985a9c6bf
pxseu <57842793+pxseu@users.noreply.github.com>
2020-03-25 13:10:47 +0100

    Update app.js

--- app.js
+++ app.js
@@ -234,7 +234,7 @@ function skip(message, serverQueue) {
 	if (!message.member.voiceChannel) return message.channel.send('You have to be in a voice channel to stop the music!');
 	if (!serverQueue) return message.channel.send('There is no song that I could skip!');
 	serverQueue.connection.dispatcher.end();
-	message.react("⏸️");
+	message.react("⏭️");
 }
 
 function stop(message, serverQueue) {
