3c03957122957b4ee9e26fc33d104852bf830722
Kuba Ellwart <gigantgigantowy@gmail.com>
2020-04-14 12:35:49 +0200

    update

--- app.js
+++ app.js
@@ -327,42 +327,40 @@ client.on('message', async message => {
 /////////////// MODERATION ///////////////////////////////////////
 
      case "ban":
-	const user = message.mentions.users.first();ts
-	const banReason = args.slice(1).join(' ');
-	if (!user) {
-		try {
-			if (!message.guild.members.get(args.slice(0, 1).join(' '))) throw new Error('Couldn\' get a Discord user with this userID!');
-			user = message.guild.members.get(args.slice(0, 1).join(' '));
-			user = user.user;
-		} catch (error) {
-			return message.reply('Couldn\' get a Discord user with this userID!');
-		}
-	}
-	if (user === message.author) return message.channel.send('You can\'t ban yourself'); // Check if the user mention or the entered userID is the message author himsmelf
-	if (!reason) return message.reply('You forgot to enter a reason for this ban!'); // Check if a reason has been given by the message author
-	if (!message.guild.member(user).bannable) return message.reply('You can\'t ban this user because you the bot has not sufficient permissions!'); // Check if the user is bannable with the bot's permissions
+      const user = message.mentions.users.first();ts
+      const banReason = args.slice(1).join(' ');
+      if (!user) {
+        try {
+          if (!message.guild.members.get(args.slice(0, 1).join(' '))) throw new Error('Couldn\' get a Discord user with this userID!');
+           user = message.guild.members.get(args.slice(0, 1).join(' '));
+           user = user.user;
+        } catch (error) {
+          return message.reply('Couldn\' get a Discord user with this userID!');
+		    }
+	    }
+	    if (user === message.author) return message.channel.send('You can\'t ban yourself');
+	    if (!reason) return message.reply('You forgot to enter a reason for this ban!');
+	    if (!message.guild.member(user).bannable) return message.reply('You can\'t ban this user because you the bot has not sufficient permissions!');
+	    await message.guild.ban(user)
+	    const banConfirmationEmbed = new Discord.RichEmbed()
+	      .setColor('RED')
+	      .setDescription(`✅ ${user.tag} has been successfully banned!`);
+	      message.channel.send({
+	      embed: banConfirmationEmbed
+	    });
 ​
-	await message.guild.ban(user) // Bans the user
-​
-	const banConfirmationEmbed = new Discord.RichEmbed()
-	.setColor('RED')
-	.setDescription(`✅ ${user.tag} has been successfully banned!`);
-	message.channel.send({
-	embed: banConfirmationEmbed
-	});
-​
-	const banConfirmationEmbedModlog = new Discord.RichEmbed()
-		.setAuthor(`Banned by **${msg.author.username}#${msg.author.discriminator}**`, msg.author.displayAvatarURL)
-		.setThumbnail(user.displayAvatarURL)
-		.setColor('RED')
-		.setTimestamp()
-		.setDescription(`**Action**: Ban
-		**User**: ${user.username}#${user.discriminator} (${user.id})
-		**Reason**: ${reason}`);
-		client.channels.get("694925675710382090").send({
-		embed: banConfirmationEmbedModlog
-	}); // Sends the RichEmbed in the modlogchannel
-	}
+	    const banConfirmationEmbedModlog = new Discord.RichEmbed()
+	      .setAuthor(`Banned by **${msg.author.username}#${msg.author.discriminator}**`, msg.author.displayAvatarURL)
+	      .setThumbnail(user.displayAvatarURL)
+	      .setColor('RED')
+	      .setTimestamp()
+	      .setDescription(`**Action**: Ban
+	      **User**: ${user.username}#${user.discriminator} (${user.id})
+	      **Reason**: ${reason}`);
+	      client.channels.get("694925675710382090").send({
+	      embed: banConfirmationEmbedModlog
+	      });
+	    return 0;
 
 /////////////// MODERATION ///////////////////////////////////////
 
