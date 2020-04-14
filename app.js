4bc1e8370bd7088bd957d954371129ad75167bd7
Kuba Ellwart <gigantgigantowy@gmail.com>
2020-04-14 12:42:24 +0200

    update

--- app.js
+++ app.js
@@ -327,22 +327,27 @@ client.on('message', async message => {
 /////////////// MODERATION ///////////////////////////////////////
 
      case "ban":
-      const user = message.mentions.users.first();
-      const banReason = args.slice(1).join(' ');
-      if (!user) {
-        try {
-          if (!message.guild.members.get(args.slice(0, 1).join(' '))) throw new Error('Couldn\' get a Discord user with this userID!');
-           user = message.guild.members.get(args.slice(0, 1).join(' '));
-           user = user.user;
-        } catch (error) {
-          return message.reply('Couldn\' get a Discord user with this userID!');
-		    }
-	    }
-	    if (user === message.author) return message.channel.send('You can\'t ban yourself');
-	    if (!reason) return message.reply('You forgot to enter a reason for this ban!');
-	    if (!message.guild.member(user).bannable) return message.reply('You can\'t ban this user because you the bot has not sufficient permissions!');
-	    await message.guild.ban(user)
-	    return 0;
+      if(message.member.roles.find(r => r.name === "Head Admin") || message.member.roles.find(r => r.name === "Mod") || message.member.roles.find(r => r.name === "OWNERS")){
+        const user = message.mentions.users.first();
+        const banReason = args.slice(1).join(' ');
+        if (!user) {
+          try {
+            if (!message.guild.members.get(args.slice(0, 1).join(' '))) throw new Error('Couldn\' get a Discord user with this userID!');
+             user = message.guild.members.get(args.slice(0, 1).join(' '));
+             user = user.user;
+          } catch (error) {
+            return message.reply('Couldn\' get a Discord user with this userID!');
+		      }
+	      }
+	      if (user === message.author) return message.channel.send('You can\'t ban yourself');
+	      if (!reason) return message.reply('You forgot to enter a reason for this ban!');
+	      if (!message.guild.member(user).bannable) return message.reply('You can\'t ban this user because you the bot has not sufficient permissions!');
+	      await message.guild.ban(user)
+	      return 0;
+      } else {
+       message.reply("You don't have the permision to use this command.")
+       return 0;
+      }
 
 /////////////// MODERATION ///////////////////////////////////////
 
