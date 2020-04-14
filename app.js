eb3258352492f86daa87e5361037d911f3e27056
Kuba Ellwart <gigantgigantowy@gmail.com>
2020-04-14 23:49:29 +0200

    update

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ client.on('message', async message => {
   if(!message.guild || message.author.bot) return;
   const guildConf = client.settings.ensure(message.guild.id, defaultSettings);
   if(message.content.indexOf(guildConf.prefix) !== 0) return;
-  if(!message.author.hasRole(roleafterver) return;
+  if(!message.author.hasRole(guildConf.roleafterver)) return;
   const args = message.content.slice(guildConf.prefix.length).trim().split(/ +/g);
   const command = args.shift().toLowerCase();
   const serverQueue = queue.get(message.guild.id);
