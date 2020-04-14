a35f95c82b6b4d461c533611bfd834c87d698248
pxseu <57842793+pxseu@users.noreply.github.com>
2020-04-14 21:36:31 +0200

    update

--- app.js
+++ app.js
@@ -537,7 +537,7 @@ const shortcode = (n) => {
     return text;
 }
 
-client.on('guildMemberAdd', (member, guildConf) => {
+client.on('guildMemberAdd', (member) => {
     if (member.user.bot || member.guild.id !== (guildConf.serverid)) return
     const token = shortcode(8)
     const welcomemsg = `Welcome to ${guild.name}! We hope you find a home here! Check out the \`#rules\` channel to make sure that we live, and as long as our goals are similar, then there’s a place at the table waiting for you. \n\n If you accept the code of conduct, please verify your agreement by replying to **this DM** with the verification phrase: \n\n\`I agree to abide by all rules. My token is ${token}.\`\n\n **This message is case-sensitive, and please include the period at the end! ** \n\nQuestions? Get at a staff member in the server or via DM.`
@@ -548,7 +548,7 @@ client.on('guildMemberAdd', (member, guildConf) => {
 
 const verifymsg = 'I agree to abide by all rules. My token is {token}.'
 
-client.on('message', (message, guildConf) => {
+client.on('message', (message) => {
     if (message.author.bot || !message.author.token || message.channel.type !== `dm`) return
     if (message.content !== (verifymsg.replace('{token}', message.author.token))) return
     message.channel.send({
