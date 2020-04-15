06ef766d7b55c9be49be6cf0b1b4f1a7a7902684
pxseu <57842793+pxseu@users.noreply.github.com>
2020-04-15 11:35:20 +0200

    update

--- app.js
+++ app.js
@@ -244,7 +244,7 @@ client.on('message', async message => {
       return 0;
 
     case "clear":
-      if(message.member.roles.find(r => r.name === "Head Admin") || message.member.roles.find(r => r.name === "Mod") || message.member.roles.find(r => r.name === "OWNERS")){
+      if(message.member.roles.find(r => r.name === "Head Admin") || message.member.roles.find(r => r.name === "Mod") || message.member.roles.find(r => r.name === "OWNERS") || message.member.roles.find(r => r.name === guildConf.adminRole) || message.member.roles.find(r => r.name === guildConf.modRole)){
        const bruh = message.content.split(' ').slice(1);
        const amount = bruh.join(' ');
 
@@ -354,7 +354,7 @@ client.on('message', async message => {
        args[0] = "";
        txt = args.join(' ');
        console.log(txt);
-       message.channel.get(channelid).send({embed: {color: 10181046, description: txt}});
+       client.channel.get(channelid).send({embed: {color: 10181046, description: txt}});
        return  0;
       } else {
        message.reply("You don't have the permision to use this command.")
