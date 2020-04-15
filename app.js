0253af546f3e42980b69818dacb31a97050a90ce
pxseu <57842793+pxseu@users.noreply.github.com>
2020-04-15 11:21:07 +0200

    update

--- app.js
+++ app.js
@@ -427,14 +427,14 @@ client.on('message', async message => {
       message.channel.send(`The following are the server's current configuration: \`\`\`${configProps}\`\`\``);
       return 0;
     case "setconf":
-       	 if(message.member.roles.find(r => r.name === guildConf.adminRole) || || message.member.roles.find(r => r.name === guildConf.modRole)) {
+       	 if(message.member.roles.find(r => r.name === guildConf.adminRole) || message.member.roles.find(r => r.name === guildConf.modRole)) {
        	   const [prop, ...value] = args;
            if(!client.settings.has(message.guild.id, prop)) {
-             return message.reply("This key is not in the configuration."); 
+             return message.reply("This key is not in the configuration.");
            }
            client.settings.set(message.guild.id, value.join(" "), prop);
            message.channel.send(`Guild configuration item ${prop} has been changed to:\n\`${value.join(" ")}\``);
-           return 0; 
+           return 0;
          }
          else {return message.reply("You're not an admin, sorry!");}
 //////////////// SETUP ///////////////////////////////////////////
