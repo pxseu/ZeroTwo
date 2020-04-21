af1ca742f44c1018b82ae7a7dfa822b6c836062a
pxseu <57842793+pxseu@users.noreply.github.com>
2020-04-21 21:18:22 +0200

    update

--- commands/skip.js
+++ commands/skip.js
@@ -3,7 +3,8 @@ module.exports = {
    description: 'skip',
    async execute(message, args, guildConf, serverQueue, queue, client, Server ) {
       if (!message.member.voiceChannel) return message.channel.send(
-         'You have to be in a voice channel to stop the music!');
+         'You have to be in a voice channel to stop the music!'
+      );
       if (!serverQueue) return message.channel.send('There is no song that I could skip!');
       await Server.updateOne({
         serverid: message.guild.id
