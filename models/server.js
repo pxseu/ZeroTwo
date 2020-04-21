62dbbb183d587dc1d0a34b84f271d0b857e83199
Kuba Ellwart <gigantgigantowy@gmail.com>
2020-04-21 10:29:44 +0200

    update

---
+++ models/server.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema;
+
+const serverSchema = new Schema({
+  prefix: String,
+  logchannel: String,
+  roleafterver: String,
+  serverid: String,
+  adminRole: String,
+  modRole: String,
+  verification: Boolean,
+  logging: Boolean
+})
+
+module.exports = mongoose.model('Server', serverSchema)
