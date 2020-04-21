99c95ab38184804a4117c413c9f098358210e1cc
Kuba Ellwart <gigantgigantowy@gmail.com>
2020-04-21 21:18:22 +0200

    update

--- commands/play.js
+++ commands/play.js
@@ -91,7 +91,8 @@ module.exports = {
       const args = message.content.split(' ');
       const voiceChannel = message.member.voiceChannel;
       if (!voiceChannel) return message.channel.send(
-         'You need to be in a voice channel to play music!');
+         'You need to be in a voice channel to play music!'
+      );
       const permissions = voiceChannel.permissionsFor(message.client.user);
       if (!permissions.has('CONNECT') || !permissions.has('SPEAK')) {
          return message.channel.send(
--- commands/stop.js
+++ commands/stop.js
@@ -1,9 +1,15 @@
 module.exports = {
    name: 'stop',
    description: 'stop',
-   execute(message, args, guildConf, serverQueue) {
+   async execute(message, args, guildConf, serverQueue, queue, client, Server) {
       if (!message.member.voiceChannel) return message.channel.send(
-         'You have to be in a voice channel to stop the music!');
+         'You have to be in a voice channel to stop the music!'
+      );
+      await Server.updateOne({
+        serverid: message.guild.id
+      }, {
+        loopsongs: false
+      })
       serverQueue.songs = [];
       serverQueue.connection.dispatcher.end();
       message.react("🛑");
