058838eff0b717f9f594f41b834c3bf624c09210
pxseu <57842793+pxseu@users.noreply.github.com>
2020-04-21 13:46:52 +0200

    update

---
+++ commands/loopsong.js
@@ -0,0 +1,25 @@
+module.exports = {
+   name: 'loopsong',
+   description: 'loop current song',
+   async execute(message, args, guildConf, serverQueue, queue, client, Server) {
+      if (!message.member.voiceChannel) {return message.channel.send(
+         'You have to be in a voice channel to stop the music!');}
+      if (!serverQueue){ return message.channel.send('There is no song that I could skip!')};
+      console.log(guildConf.loopsongs)
+      if (guildConf.loopsongs == false){   
+         await Server.updateOne({
+            serverid : message.guild.id
+         }, {
+            loopsong : true
+         });
+         message.react("🔄");
+      } else {
+         await Server.updateOne({
+            serverid: message.guild.id
+         }, {
+            loopsong: false
+         });
+         message.react("🛑");
+      }
+   }
+}
--- commands/skip.js
+++ commands/skip.js
@@ -1,10 +1,15 @@
 module.exports = {
    name: 'skip',
    description: 'skip',
-   execute(message, args, guildConf, serverQueue, ) {
+   async execute(message, args, guildConf, serverQueue, ) {
       if (!message.member.voiceChannel) return message.channel.send(
          'You have to be in a voice channel to stop the music!');
       if (!serverQueue) return message.channel.send('There is no song that I could skip!');
+      await Server.updateOne({
+        serverid: message.guild.id
+      }, {
+        loopsong: false
+      });
       serverQueue.connection.dispatcher.end();
       message.react("⏭️");
    }
