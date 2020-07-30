b20e343fa3a829a9936d1a5efb3760af3deb151c
Kuba Ellwart <57842793+Modman276@users.noreply.github.com>
2020-07-30 21:47:49 +0200

    update

--- commands/help.js
+++ commands/help.js
@@ -2,9 +2,7 @@ module.exports = {
    name: 'help',
    description: 'Halp me',
    execute(message, args) {
-      const {
-         embedhelp
-      } = require('../config.json');
+      
       const embed = embedhelp
       message.channel.send({
          embed
