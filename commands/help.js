5201f95b121ec5dc2a2ee7f8c5aa76f8cb3beedd
pxseu <57842793+pxseu@users.noreply.github.com>
2020-07-30 21:56:36 +0200

    update

--- commands/help.js
+++ commands/help.js
@@ -1,11 +1,23 @@
+const { MessageEmbed } = require('discord.js');
+
 module.exports = {
    name: 'help',
    description: 'Halp me',
-   execute(message, args) {
-      
-      const embed = embedhelp
-      message.channel.send({
-         embed
+   execute(message, args, guildConf, serverQueue, queue, client) {
+         let user = message.member;
+
+         const embed = new MessageEmbed()
+         .setAuthor(user.user.username, user.user.displayAvatarURL({ dynamic: true }))
+         .setColor("RANDOM")
+         .setThumbnail(user.user.displayAvatarURL())
+         .setTitle("**Results:**")
+         .setFooter(message.guild.name, message.guild.iconURL())
+         .setTimestamp()
+
+      client.commands.forEach(command => {
+         embed.addField(command.name, command.description)
       });
+      
+      return message.channel.send(embed);
    },
 };
