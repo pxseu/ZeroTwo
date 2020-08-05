88e110e7e007ba4c19761597ef72eb318d64a453
pxseu <57842793+pxseu@users.noreply.github.com>
2020-08-05 20:46:24 +0200

    update

--- commands/ping.js
+++ commands/ping.js
@@ -1,7 +1,32 @@
+const Discord = require("discord.js")
+
 module.exports = {
    name: 'ping',
    description: 'Ping!',
    execute(message, args) {
-      message.reply('Pong.');
+      let embed, ping;
+
+      embed = new Discord.MessageEmbed({
+         title: "Ping",
+         description: "<a:loading:521018476480167937> Pinging..."
+      });
+
+      message.channel.send(embed).then(msg => {
+
+         ping = msg.createdTimestamp - message.createdTimestamp;
+         let color = 
+            ping < 250 ? "#00ff00" : 
+            ping > 250 && ping < 500 ? 
+               "#ffaa00" : "#ff0000";
+
+         embed.setDescription(
+            `**You** > **Discord** (\`\`${Math.floor(ping)}ms\`\`)
+            **We** > **Discord API** (\`\`${Math.floor(message.client.ws.ping)}ms\`\`)`
+         ).setColor(color);
+
+         msg.edit(embed).then(msg => msg.delete({ timeout: 15 * 1000 }));
+      });
+
+      if (!message.deleted) message.delete();
    }
 }
