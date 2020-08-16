9ec02e715ab04de9110e0aaeb37c634a0b8c01ce
Kuba Ellwart <57842793+Modman276@users.noreply.github.com>
2020-08-16 23:01:02 +0200

    update

--- commands/vibe.js
+++ commands/vibe.js
@@ -6,8 +6,8 @@ module.exports = {
    execute(message, args) {
       const embed = new MessageEmbed();
       embed.setColor("RANDOM");
-      embed.setDescription(`Zero Two vibes with${message.author.id}.`);
-      embed.setImage("https://image.myanimelist.net/ui/G-Sm6d0qIwQxUGHIp-m2WE4r0RSD61OQcp0zIes03ZCYoKjsVsjXKaeievJ3JFbIPWVFdDFNffxoioO0_wZFCqs4E0_YgZYsXqmLfTNB-IZA-B-IvlYVs7FcQAbSVU5Z");
+      embed.setDescription(`Zero Two vibes with <@${message.author.id}>.`);
+      embed.setImage("https://64.media.tumblr.com/c98bae671e32921caeaa6e9ef9234015/tumblr_p8zk1gXu2y1t0lt8go1_540.gif");
       message.channel.send(embed)
    },
    type: 3
