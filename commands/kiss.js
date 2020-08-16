e7dfd22e2ff7741e874bc7d4e47d1b170eb11de7
pxseu <57842793+pxseu@users.noreply.github.com>
2020-08-16 22:53:07 +0200

    update

---
+++ commands/kiss.js
@@ -0,0 +1,27 @@
+const { MessageEmbed } = require('discord.js');
+
+module.exports = {
+   name: 'kiss',
+   description: 'Kiss!',
+   execute(message, args) {
+      const {
+         kissgifs
+      } = require('../config.json');
+      const kiss = kissgifs[Math.floor(Math.random() * kissgifs.length)];
+
+      let msgContent;
+      const tagged = message.mentions.members.first() || message.guild.members.cache.get(args[0]) || message.guild.members.cache.find(r => r.user.username.toLowerCase() === args.join(' ').toLocaleLowerCase()) || message.guild.members.cache.find(ro => ro.displayName.toLowerCase() === args.join(' ').toLocaleLowerCase());
+
+      if (tagged != undefined && tagged.id != message.author.id){
+         msgContent = `<@${message.author.id}> hugs <@${tagged.id}> tightly (´・ω・｀)`;
+      } else {
+         msgContent = `hugs back <@${message.author.id}> tightly (´・ω・｀)`;
+      }
+      const embed = new MessageEmbed();
+      embed.setColor("RANDOM");
+      embed.setDescription(msgContent)
+      embed.setImage(kiss)
+      message.channel.send(embed);
+   },
+   type: 3
+};
--- commands/vibe.js
+++ commands/vibe.js
@@ -1,16 +1,14 @@
+const { MessageEmbed } = require("discord.js")
+
 module.exports = {
    name: 'vibe',
    description: 'Vibin.',
    execute(message, args) {
-      message.channel.send({
-         embed: {
-            color: 10181046,
-            title: "Zero Two vibes with " + message.author.username + ".",
-            image: {
-               url: "https://image.myanimelist.net/ui/G-Sm6d0qIwQxUGHIp-m2WE4r0RSD61OQcp0zIes03ZCYoKjsVsjXKaeievJ3JFbIPWVFdDFNffxoioO0_wZFCqs4E0_YgZYsXqmLfTNB-IZA-B-IvlYVs7FcQAbSVU5Z"
-            }
-         }
-      })
+      const embed = new MessageEmbed();
+      embed.setColor("RANDOM");
+      embed.setDescription(`Zero Two vibes with${message.author.id}.`);
+      embed.setImage("https://image.myanimelist.net/ui/G-Sm6d0qIwQxUGHIp-m2WE4r0RSD61OQcp0zIes03ZCYoKjsVsjXKaeievJ3JFbIPWVFdDFNffxoioO0_wZFCqs4E0_YgZYsXqmLfTNB-IZA-B-IvlYVs7FcQAbSVU5Z");
+      message.channel.send(embed)
    },
    type: 3
 }
