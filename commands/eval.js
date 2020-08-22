e816bb6b3ea88483f98b86695800acfe98500da9
pxseu <57842793+pxseu@users.noreply.github.com>
2020-08-22 18:31:44 +0200

    update

--- commands/eval.js
+++ commands/eval.js
@@ -2,27 +2,26 @@ const { ownerid } = require("../config.json");
 
 module.exports = {
     name: 'eval',
-    description: 'Dev Eval',
+    description: 'Dev Eval (for mr. pxseu)',
     execute(message, args) {
-        if (message.author.id != ownerid) return message.channel.send('This command is for the owner only.');
-        try {
-            const code = args.join(" ");
-            let evaled = eval(code);
-       
-            if (typeof evaled !== "string")
-              evaled = require("util").inspect(evaled);
-       
-            message.channel.send(clean(evaled), {code:"xl"});
-          } catch (err) {
-            message.channel.send(`\`ERROR\` \`\`\`xl\n${clean(err)}\n\`\`\``);
-          }
+      if (message.author.id != ownerid) return message.channel.send('This command is for the owner only.');
+      try {
+          const code = args.join(" ");
+          let evaled = eval(code);
+      
+          if (typeof evaled !== "string")
+            evaled = require("util").inspect(evaled);
+          message.channel.send(clean(evaled), {code:"xl"});
+      } catch (err) {
+        message.channel.send(`\`ERROR\` \`\`\`xl\n${clean(err)}\n\`\`\``);
+      }
     },
     type: 0
 };
 
 function clean(text) {
-    if (typeof(text) === "string")
-      return text.replace(/`/g, "`" + String.fromCharCode(8203)).replace(/@/g, "@" + String.fromCharCode(8203));
-    else
-        return text;
+  if (typeof(text) === "string")
+    return text.replace(/`/g, "`" + String.fromCharCode(8203)).replace(/@/g, "@" + String.fromCharCode(8203));
+  else
+    return text;
 }
