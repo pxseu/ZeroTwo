6074d436661a01c87fb7d8e352cccfa01804b6ba
pxseu <57842793+pxseu@users.noreply.github.com>
2020-08-22 18:21:54 +0200

    update

--- commands/owner.js
+++ commands/owner.js
@@ -1,11 +1,12 @@
 const { MessageEmbed } = require("discord.js");
-
+const { ownerid } = require("../config.json");
+ 
 module.exports = {
 	name: 'owner',
 	description: 'Owner!',
 	execute(message, args) {
 		const embed = new MessageEmbed();
-		embed.setDescription(`My owner is \`\`pxseu#0001\`\`\n[My owners website](https://www.pxseu.com)`);
+		embed.setDescription(`My owner is \`\`pxseu#0001\`\`\n (${ownerid})[My owners website](https://www.pxseu.com)`);
 		message.channel.send(embed);
 	},
 	type: 0
