229d4872dc1fb67a89df3f098c6a47fe81562dc3
pxseu <57842793+pxseu@users.noreply.github.com>
2020-08-23 13:44:43 +0200

    update

--- commands/avatar.js
+++ commands/avatar.js
@@ -4,16 +4,15 @@ module.exports = {
    name: 'avatar',
    description: 'show user avatars',
    execute(message, args) {
-      let user = message.mentions.members.first()
+      let member = message.mentions.members.first()
       || message.guild.members.cache.get(args[0])
       || message.guild.members.cache.find(r => r.user.username.toLowerCase() === args.join(' ').toLocaleLowerCase())
       || message.guild.members.cache.find(ro => ro.displayName.toLowerCase() === args.join(' ').toLocaleLowerCase())
       || message.member;
-      user = user.user;
-      
+   
       const embed = new MessageEmbed();
-      embed.setDescription(`${ user.id == message.author.id ? "Your" : `${user.username}'s`} avatar`)
-      embed.setImage(user.displayAvatarURL({ dynamic: true }));
+      embed.setDescription(`${ member.user.id == message.author.id ? "Your" : `${member.displayName ? member.displayName : member.user.username}'s`} avatar`)
+      embed.setImage(member.user.displayAvatarURL({ dynamic: true }));
 
       message.channel.send(embed);
    },
