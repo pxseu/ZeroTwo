677aded82095e04d2856d5443e4c215569a83706
pxseu <57842793+pxseu@users.noreply.github.com>
2020-08-29 00:10:28 +0200

    update

---
+++ utils/config.js
@@ -0,0 +1,43 @@
+const commandsCattegories = {
+	0: 'Commands for bot',
+	1: 'User commands',
+	2: 'Utility commands',
+	3: 'Fun commands',
+	4: 'Music commands',
+};
+
+const patgifs = [
+	'https://i.imgur.com/UWbKpx8.gif',
+	'https://thumbs.gfycat.com/LightOilyIraniangroundjay-size_restricted.gif',
+	'https://thumbs.gfycat.com/FlimsyDeafeningGrassspider-size_restricted.gif',
+	'https://thumbs.gfycat.com/ImpurePleasantArthropods-small.gif',
+	'https://i.pinimg.com/originals/c0/c1/c5/c0c1c5d15f8ad65a9f0aaf6c91a3811e.gif',
+];
+
+const huggifs = [
+	'https://media.giphy.com/media/ddGxYkb7Fp2QRuTTGO/giphy.gif',
+	'https://media.giphy.com/media/lrr9rHuoJOE0w/giphy.gif',
+	'https://i.pinimg.com/originals/74/b6/ae/74b6ae32345aad709bb0e41a6f867626.gif',
+	'https://media.giphy.com/media/vTtibSrt4dlIc/giphy.gif',
+	'https://media.giphy.com/media/ZQN9jsRWp1M76/giphy.gif',
+	'https://media.giphy.com/media/u9BxQbM5bxvwY/giphy.gif',
+];
+
+const kissgifs = [
+	'https://media2.giphy.com/media/bGm9FuBCGg4SY/giphy.gif',
+	'https://i.imgur.com/i1PIph3.gif',
+];
+
+const bypassIds = [
+	'338718840873811979',
+	'670600281846841354',
+	'343627503698706432',
+];
+
+module.exports = {
+	commandsCattegories,
+	bypassIds,
+	kissgifs,
+	patgifs,
+	huggifs,
+};
