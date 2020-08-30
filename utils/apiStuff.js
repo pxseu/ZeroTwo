da66dd64d13cb153bfd1bc69e9e5f5e974d67391
pxseu <57842793+pxseu@users.noreply.github.com>
2020-08-30 22:59:49 +0200

    update

--- utils/apiStuff.js
+++ utils/apiStuff.js
@@ -8,10 +8,11 @@ const getImage = (endpoint = "") =>
 
 		const request = await fetch(`https://nekos.life/api/v2/img${endpoint}`);
 
-		if (request.status != 200)
+		const data = await request.json();
+
+		if (request.status != 200 || data == undefined || data.url == undefined)
 			return reject(new Error("Error while fetching data"));
 
-		const data = await request.json();
 		resolve(data);
 	});
 
