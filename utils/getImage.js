e1aef6137a91c0c3498d34b28d68068afe9398f4
pxseu <ellwartkuba@gmail.com>
2020-08-30 13:33:22 +0200

    lao

--- utils/getImage.js
+++ utils/getImage.js
@@ -1,6 +1,10 @@
 const fetch = require("node-fetch");
 
 async function getImage(endpoint = "") {
+	if (endpoint == undefined || endpoint == "") {
+		throw new Error("No endpoint defined");
+	}
+
 	return await fetch(`https://nekos.life/api/v2/img${endpoint}`).then((res) =>
 		res.json()
 	);
