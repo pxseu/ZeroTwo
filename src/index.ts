13ad45e95d580d112878ca25834922405da6d58f
pxseu <57842793+pxseu@users.noreply.github.com>
2020-11-18 22:55:35 +0100

    ✨ better code ql

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,7 @@ import music from "./utils/music";
 import guildStuff from "./utils/guildStuff";
 import mainMessageHandler from "./utils/mainMessageHandler";
 
+export const DEV_MODE = process.env.NODE_ENV != "production";
 export const client = new Client();
 
 client.commands = new Collection();
