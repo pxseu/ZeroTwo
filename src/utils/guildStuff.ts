1b7baa87696c9f1bea7f611552f716ced533b773
pxseu <57842793+pxseu@users.noreply.github.com>
2020-11-18 22:55:35 +0100

    ✨ better code ql

--- src/utils/guildStuff.ts
+++ src/utils/guildStuff.ts
@@ -1,9 +1,24 @@
 import Server from "../models/server";
 import events from "../utils/events";
 
-import { client } from "../";
+import { client, DEV_MODE } from "../";
 import { owner } from "./config";
-import { MessageEmbed } from "discord.js";
+import { Guild, MessageEmbed } from "discord.js";
+
+const newServer = (guild: Guild) => {
+	let desc = "Konnichiwa ( ´ ▽ ` )\n";
+	desc += "Thank you for adding me!\n";
+	desc += "Type zt!help for commands.";
+
+	const embed = new MessageEmbed();
+	embed.setDescription(desc);
+	embed.setColor(DEV_MODE ? "#FF00FF" : "#00FFFF");
+	guild.owner.send(embed);
+
+	new Server({
+		serverid: guild.id,
+	}).save();
+};
 
 const guildStuff = () => {
 	client.on(events.GUILDDELETE, async (guild) => {
@@ -14,15 +29,7 @@ const guildStuff = () => {
 		).deleteOne();
 	});
 
-	client.on(events.GUILDCREATE, (guild) => {
-		guild.owner.send(
-			`Konnichiwa ( \´ ▽ \` )\nThank you for adding me!\nType zt!help for commands.`,
-		);
-
-		new Server({
-			serverid: guild.id,
-		}).save();
-	});
+	client.on(events.GUILDCREATE, newServer);
 
 	client.on(events.READY, () => {
 		console.log(`> Logged in as ${client.user.tag}!`);
@@ -41,13 +48,7 @@ const guildStuff = () => {
 			});
 
 			if (server == undefined) {
-				guild.owner.send(
-					`Konnichiwa ( \´ ▽ \` )\nThank you for adding me!\nType zt!help for commands.`,
-				);
-
-				new Server({
-					serverid: guild.id,
-				}).save();
+				newServer(guild);
 			}
 
 			const serversInDb = await Server.find();
@@ -72,7 +73,7 @@ const guildStuff = () => {
 		description += `\`\`\``;
 
 		const embed = new MessageEmbed();
-		embed.setColor("RANDOM");
+		embed.setColor(DEV_MODE ? "#FF00FF" : "#00FFFF");
 		embed.setTitle("Bot started!");
 		embed.setDescription(description);
 		embed.setTimestamp();
