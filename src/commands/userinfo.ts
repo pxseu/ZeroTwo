856d61b3f0a89d012931160e0ec937d381c70e2e
pxseu <57842793+pxseu@users.noreply.github.com>
2020-11-25 10:15:55 +0100

    commit with no test

--- src/commands/userinfo.ts
+++ src/commands/userinfo.ts
@@ -22,18 +22,20 @@ module.exports = {
 		}
 
 		let member =
-			message.mentions.members.first() ||
-			message.guild.members.cache.get(args[0]) ||
-			message.guild.members.cache.find(
-				(r) =>
-					r.user.username.toLowerCase() ===
-					args.join(" ").toLocaleLowerCase(),
-			) ||
-			message.guild.members.cache.find(
-				(ro) =>
-					ro.displayName.toLowerCase() ===
-					args.join(" ").toLocaleLowerCase(),
-			);
+			user.id == message.author.id
+				? message.member
+				: message.mentions.members.first() ||
+				  message.guild.members.cache.get(args[0]) ||
+				  message.guild.members.cache.find(
+						(r) =>
+							r.user.username.toLowerCase() ===
+							args.join(" ").toLocaleLowerCase(),
+				  ) ||
+				  message.guild.members.cache.find(
+						(ro) =>
+							ro.displayName.toLowerCase() ===
+							args.join(" ").toLocaleLowerCase(),
+				  );
 
 		const roles = member
 			? member.roles.cache.map((r) => `${r}`).join(" | ")
