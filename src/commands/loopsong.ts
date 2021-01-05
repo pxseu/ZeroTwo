dbbec46331e560d63ed23df18eb2953d774ed0ac
pxseu <57842793+pxseu@users.noreply.github.com>
2021-01-05 14:32:57 +0100

    updates to deps

--- src/commands/loopsong.ts
+++ src/commands/loopsong.ts
@@ -20,13 +20,11 @@ module.exports = {
 		const repeatMode = message.client.player.getQueue(message).repeatMode;
 
 		if (repeatMode) {
-			//@ts-expect-error Again wrong typings
 			message.client.player.setRepeatMode(message, false);
 			message.info("Repeat mode disabled.");
 			return;
 		}
 
-		//@ts-expect-error Again wrong typings
 		message.client.player.setRepeatMode(message, true);
 		message.info("Repeat mode enabled.");
 	},
