ce771ef8635691c114bcfdec3d21206e5cc41e29
pxseu <57842793+pxseu@users.noreply.github.com>
2021-01-10 03:56:36 +0100

    uwu

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -128,3 +128,25 @@ export const warnGifs = Object.freeze([
 	"https://media.tenor.com/images/8aadd110ebcb4e714843a614fcbe2ee2/tenor.gif",
 	"https://media.tenor.com/images/07778c672663d144c8ae4359de86db0f/tenor.gif",
 ]);
+
+export const warnDesc = Object.freeze([
+	`Q. Have any friends 
+        A. Yes 
+        Q. What's your gender 
+        A. Male 
+        Q. Favorite hobbies besides games 
+        A. Guitar 
+        Q. What happens if your offline 
+        A. Internet connection 
+        If you want to add me on steam comment below and have a really good reson or I will DECLINE and I DON'T ACCPET PRIVET PROFILES but IDC if you have a or multipule VAC bans as long I see your profile then I will accpet.
+        Online: feel free to talk to me or invite me to a game that I have 
+        Away: at school or guitar lessons 
+        Busy: cant do Jackshit right now or eating dinner 
+        Looking to play: just invite me to a game what ever I'm doing
+        Looking to trade: you'er welcome to trade me 
+        Offline: Dont have internet or parentes are yelling 
+        at me 
+        In game: don't bother talking to me or inviting me to a game 
+		and if I dont find respect in the comment secion YOU WILL BE BLOCKED and if you disrespect in my group after you join me or the other admins but if you post something funny and everyone smiles at it then you can stay in I also have a discord server so if you want to join you can same rules but one rule in my discord DON'T SPAM MEMES and Porno`,
+	`Not gonna be active on Dlscord tonight. I'm meeting a girl (a real one) in half an hour (wouldn't expect a lot of you to understand anyway) so please don't DM me asking me where I am (im with the girl, ok) you'll most likely get aired because ill be with the girl (again I don't expect you to understand) shes actually really interested in me and its not a situation i can pass up for some meaningless Dlscord degenerates (because ill be meeting a girl, not that you really are going to understand) this is my life now. Meeting women and not wasting my precious time online, I have to move on from such simple things and branch out (you wouldnt understand)`,
+]);
