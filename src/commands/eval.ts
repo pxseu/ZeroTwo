9bb1525420d0ac2d3dc1e8725a30b0d84d72bfd3
pxseu <57842793+pxseu@users.noreply.github.com>
2021-01-12 17:37:18 +0100

    api wrapper test

--- src/commands/eval.ts
+++ src/commands/eval.ts
@@ -3,8 +3,10 @@ import { bypassIds, embedColor } from "../utils/config";
 import { MessageEmbed } from "discord.js";
 import { inspect } from "util";
 import { Util } from "discord.js";
-import fetch from "../utils/fetchWrapper";
 import { Message } from "discord.js";
+import { Imperial } from "imperial-node";
+
+const { postCode } = new Imperial(process.env.IMPERIAL_TOKEN);
 
 module.exports = {
 	name: "eval",
@@ -53,59 +55,18 @@ function h() {
 	return "h";
 }
 
-interface imperialResponse {
-	success: boolean;
-	documentId: string;
-	rawLink: string;
-	formattedLink: string;
-}
-
-class FetchErrors extends Error {
-	constructor(public message: string) {
-		super(message);
-	}
-}
-
 async function isTooLong(message: Message, text: string): Promise<void> {
 	if (text.length < 2001) {
 		message.channel.send(text, { code: "xl" });
 		return;
 	}
-
 	message.channel.startTyping();
 
 	try {
-		const response = await fetch("https://www.imperialb.in/api/postCode", {
-			method: "POST",
-			headers: {
-				"Content-Type": "application/x-www-form-urlencoded",
-			},
-			redirect: "follow",
-			body: new URLSearchParams({
-				code: text,
-			}),
-		});
-
-		if (!response.ok || response.status != 200) {
-			throw new FetchErrors("Failed to fetch.");
-		}
-
-		const api: imperialResponse = await response.json();
-
-		if (!api.success || !api.formattedLink) {
-			throw new FetchErrors("Invalid response from the api.");
-		}
-
-		message.info(`Message was too long: <${api.formattedLink}>`);
+		const response = await postCode(text);
+		message.info(`Message was too long: <${response.formattedLink}>`);
 	} catch (error) {
-		if (error.type == "invalid-json") {
-			message.error("Failed to parse the response.");
-		} else if (error instanceof FetchErrors) {
-			message.error(error.message);
-		} else {
-			console.log(error);
-			message.error("Unkown fetch error.");
-		}
+		message.error(error.message ?? "Unknown");
 	}
 	message.channel.stopTyping();
 }
