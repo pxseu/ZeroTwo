877d86b75eccc341bd8c9fc3bc6ca4545929f75b
pxseu <57842793+pxseu@users.noreply.github.com>
2021-01-18 00:10:10 +0100

    better filter

--- src/utils/commands/commandLoader.ts
+++ src/utils/commands/commandLoader.ts
@@ -19,6 +19,17 @@ const loadCommands = async (): Promise<Collection<string, Command>> => {
 	return commands;
 };
 
+/**
+ * Recursively walk a directory asynchronously and obtain all file names (with full path).
+ *
+ * @param dir Folder name you want to recursively process
+ * @param done Callback function, returns all files with full path.
+ * @param filter Optional filter to specify which files to include,
+ *   e.g. for json files: (f: string) => /.json$/.test(f)
+ *  @copyright https://gist.github.com/erikvullings/c7eed546a4be0ba43532f8b83048ef38
+ *
+ */
+
 const walk = (dir: string, done: (err: Error | null, results?: string[]) => void, filter?: (f: string) => boolean) => {
 	let results: string[] = [];
 	readdir(dir, (err: Error, list: string[]) => {
@@ -61,16 +72,16 @@ const walk = (dir: string, done: (err: Error | null, results?: string[]) => void
 function getCommandFiles(dir: string): Promise<string[]> {
 	const startPath = "../../";
 	return new Promise((resolve, reject) => {
-		walk(dir, (e, d) => {
-			if (e) return reject(e);
-			const relativePath = path.resolve(__dirname, startPath);
+		walk(
+			dir,
+			(e, d) => {
+				if (e) return reject(e);
+				const relativePath = path.resolve(__dirname, startPath);
 
-			resolve(
-				d
-					.map((path) => startPath + path.slice(relativePath.length + 1, path.length))
-					.filter((file) => file.endsWith(".js"))
-			);
-		});
+				resolve(d.map((path) => startPath + path.slice(relativePath.length + 1, path.length)));
+			},
+			(file) => /.js$/.test(file)
+		);
 	});
 }
 
