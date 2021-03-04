759470b9285557d49caea23e2c83a93af98e0ce6
pxseu <57842793+pxseu@users.noreply.github.com>
2021-03-04 15:52:03 +0100

    rgiohdfgiohjdfgpij

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
+import "dotenv/config";
 import "./utils/extenders";
 
-import { config } from "dotenv";
 import { Client } from "discord.js";
 import database from "./utils/bot/database";
 import guildStuff from "./utils/bot/guildStuff";
@@ -9,27 +9,7 @@ import mainMessageHandler from "./utils/commands/mainMessageHandler";
 import player from "./utils/bot/player";
 import loadCommands from "./utils/commands/commandLoader";
 
-config();
-
 export const client = new Client();
-
-setInterval(() => {
-	let totalSeconds = client.uptime / 1000;
-	const days = Math.floor(totalSeconds / 86400);
-	totalSeconds %= 86400;
-	const hours = Math.floor(totalSeconds / 3600);
-	totalSeconds %= 3600;
-	const minutes = Math.floor(totalSeconds / 60);
-	const seconds = Math.floor(totalSeconds % 60);
-
-	let uptime = days > 0 ? `${days} days, ` : "";
-	uptime += hours > 0 ? `${hours} hours, ` : "";
-	uptime += minutes > 0 ? `${minutes} minutes, ` : "";
-	uptime += seconds > 0 ? `${seconds} seconds` : "";
-	uptime = uptime.trimEnd();
-	uptime;
-	//console.log(uptime);
-}, 200);
 
 process.on("uncaughtException", function (err) {
 	console.log("Caught exception: " + err);
