adb9efeb42f06014d3694f65d4e7e3d90e1767f8
pxseu <57842793+pxseu@users.noreply.github.com>
2022-06-09 22:33:13 +0200

    feat: fix say to work in dms

--- src/commands/bot/say.ts
+++ src/commands/bot/say.ts
@@ -4,6 +4,7 @@ import { ArgumentDefinition, OptionTypes, SubCommand } from "../../classes/Comma
 export default class BotSay extends SubCommand {
 	public name = "say";
 	public description = "Say something in the channel";
+	public ephermal = true;
 	public options: ArgumentDefinition[] = [
 		{
 			name: "text",
@@ -17,15 +18,7 @@ export default class BotSay extends SubCommand {
 		const text = args?.find((arg) => arg.name === this.options[0].name)?.value as string;
 		const regex = /^i('?|\s+a)m\s+((so+|very|really)\s+)?(stupid|dum+b?)$/gi;
 
-		if (!interaction.inGuild())
-			return interaction.editReply({
-				embeds: [
-					this.client._zerotwo.embed({
-						description: "Why would you say that about yourself :(",
-						color: this.client._zerotwo.colors.toNumber("red"),
-					}),
-				],
-			});
+		const channel = interaction.channel ?? (await interaction.user.createDM());
 
 		if (regex.test(text.trim())) {
 			return interaction.editReply({
@@ -38,11 +31,15 @@ export default class BotSay extends SubCommand {
 			});
 		}
 
-		interaction.channel!.send(
-			Util.cleanContent(text, interaction.channel!).replaceAll(
-				/@(everyone|here)/gi,
-				(_, mention) => `@${String.fromCharCode(8203)}${mention}`,
-			),
-		);
+		channel.send(Util.cleanContent(text, interaction.channel!));
+
+		return interaction.editReply({
+			embeds: [
+				this.client._zerotwo.embed({
+					description: "Message sent!",
+					color: this.client._zerotwo.colors.toNumber("pink"),
+				}),
+			],
+		});
 	}
 }
